3ddd4ec580ecdf9e7e21bb76a0dfc6e284f680cc
«User» <9323354@gmail.com>
2022-09-17 21:07:43 +0300

    Added folder Example011. Решили задачу с массивом с помощью методов

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,28 @@
+﻿//массив по умолчанию наполнен нулями, поэтому, чтобы его заполнить - мы воспользуемся методом, который сами напишем
+void FillArray(int[] collection)
+{
+    int length = collection.Length; // получаем длину массива
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // новое случайное целое число из диапазона 1-10
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //Метод, который печатает массив
+{
+    int count = col.Length; //Количество элементов
+    int position = 0; //Текущая позиция
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position ++;
+    }
+
+}
+
+int[] array = new int [10]; // определим новый масив и укажем, что в нем 10 элементов
+
+FillArray(array);
+PrintArray(array);
